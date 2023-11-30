31995e9932438562679dc6e2d657fb7ab07acc55
Anang Suwasto <anang.suwasto@privyid.tech>
2023-11-30 22:59:14 +0700

    setup dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -40,9 +40,5 @@ android {
 
 dependencies {
     implementation(projects.shared)
-    implementation(libs.compose.ui)
-    implementation(libs.compose.ui.tooling.preview)
-    implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
-    debugImplementation(libs.compose.ui.tooling)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,7 @@ plugins {
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
     alias(libs.plugins.kotlinCocoapods).apply(false)
+    alias(libs.plugins.spotless)
+    alias(libs.plugins.jvm) apply false
+    alias(libs.plugins.compose.multiplatform)
 }
