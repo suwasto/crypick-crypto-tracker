fa1fca3d958b5b7676333685a544f5f3ba639eda
Anang Suwasto <anang.suwasto@privyid.tech>
2023-12-12 20:47:19 +0700

    add tabs

--- shared/src/commonMain/kotlin/app/id/crypick/features/components/CrypickTab.kt
+++ shared/src/commonMain/kotlin/app/id/crypick/features/components/CrypickTab.kt
@@ -20,7 +20,7 @@ internal sealed class CrypickTab {
             @Composable
             get() {
                 val title = "Feeds"
-                val icon = painterResource("home_outlined.xml")
+                val icon = painterResource("news_outlined.xml")
                 return remember {
                     TabOptions(
                         index = 0u,
@@ -31,7 +31,7 @@ internal sealed class CrypickTab {
             }
     }
 
-    internal object OtherTab : Tab {
+    internal object CoinTab : Tab {
         @Composable
         override fun Content() {
             FeedsScreen()
@@ -41,11 +41,11 @@ internal sealed class CrypickTab {
         override val options: TabOptions
             @Composable
             get() {
-                val title = "Other"
-                val icon = painterResource("home_outlined.xml")
+                val title = "Coins"
+                val icon = painterResource("coin_outlined.xml")
                 return remember {
                     TabOptions(
-                        index = 0u,
+                        index = 1u,
                         title = title,
                         icon = icon,
                     )
@@ -53,4 +53,58 @@ internal sealed class CrypickTab {
             }
     }
 
+    internal object BookmarksTab : Tab {
+        @Composable
+        override fun Content() {
+            FeedsScreen()
+        }
+
+        @OptIn(ExperimentalResourceApi::class)
+        override val options: TabOptions
+            @Composable
+            get() {
+                val title = "Bookmarks"
+                val icon = painterResource("bookmark_outlined.xml")
+                return remember {
+                    TabOptions(
+                        index = 2u,
+                        title = title,
+                        icon = icon,
+                    )
+                }
+            }
+    }
+
+    internal object SettingsTab : Tab {
+        @Composable
+        override fun Content() {
+            FeedsScreen()
+        }
+
+        @OptIn(ExperimentalResourceApi::class)
+        override val options: TabOptions
+            @Composable
+            get() {
+                val title = "Settings"
+                val icon = painterResource("settings_outlined.xml")
+                return remember {
+                    TabOptions(
+                        index = 3u,
+                        title = title,
+                        icon = icon,
+                    )
+                }
+            }
+    }
+
+}
+
+@Composable
+@OptIn(ExperimentalResourceApi::class)
+fun FilledIcon(item: Tab) = when (item.options.index) {
+    (0u).toUShort() -> painterResource("news_filled.xml")
+    (1u).toUShort() -> painterResource("coin_filled.xml")
+    (2u).toUShort() -> painterResource("bookmark_filled.xml")
+    (3u).toUShort() -> painterResource("settings_filled.xml")
+    else -> painterResource("news_filled.xml")
 }
